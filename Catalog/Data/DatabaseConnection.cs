0615f20564a86c4b5ace4cc1fdf40e8a387bdfe0
Mihai <65979367+MihaiM21@users.noreply.github.com>
2025-05-24 15:58:44 +0300

    Update DatabaseConnection.cs

--- Catalog/Data/DatabaseConnection.cs
+++ Catalog/Data/DatabaseConnection.cs
@@ -14,7 +14,7 @@ namespace StudentGradeManagement.Data
         // Numele bazei de date = catalog_note
         // User = postgres (sau username-ul ales)
         // Password = <parola pentru user-ul postgres>
-        private static readonly string ConnectionString = "Host=localhost;Port=5432;Database=catalog_note;Username=postgres;Password=CodyCody21";
+        private static readonly string ConnectionString = "Host=localhost;Port=5432;Database=catalog_note;Username=postgres;Password=parola";
         
         public static NpgsqlConnection GetConnection()
         {
